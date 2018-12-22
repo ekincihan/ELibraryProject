cd9f5d35e4315567fdaeffbcb034d80a81e5ddf9
Furkan Çakır <furkan2834@gmail.com>
2018-12-22 20:49:36 +0300

    regular expressions added

--- ELibraryPortal/ELibrary.API/Models/TagModel.cs
+++ ELibraryPortal/ELibrary.API/Models/TagModel.cs
@@ -1,6 +1,7 @@
 ﻿using ELibrary.API.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace ELibrary.API.Models
         {
             Id = Guid.Empty;
         }
+        [RegularExpression("^((?!^Name$)[a-zA-Z '])+$", ErrorMessage = "Girdiğiniz isim formatı yanlış!")]
         public string Name { get; set; }
         public bool IsActive { get; set; } = true;
     }
