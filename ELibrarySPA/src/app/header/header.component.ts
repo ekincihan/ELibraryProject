5b0b1a40a7de584e5ef9cf2002b4edcc9a535be3
Delil Noyan <33246807+noyandelil@users.noreply.github.com>
2018-12-30 05:49:12 +0300

    Publisher Eklendi
    Publisher UI Eklendi

--- ELibrarySPA/src/app/header/header.component.ts
+++ ELibrarySPA/src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderService } from './shared/headerService';
 import { Category } from '../models/Category';
-
+import { Publisher } from '../models/Publisher';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,10 +11,18 @@ export class HeaderComponent implements OnInit {
 
   constructor(private headerService:HeaderService) { }
   categories :Category[];
+  publishers:Publisher[];
   ngOnInit() {
     this.headerService.getAll('Category/List').subscribe((res) =>{
       this.categories=res['value'];
       console.log('kategor',res['value'])
+      
+      this.headerService.getAll('Publisher/List').subscribe((rest)=>{
+        this.publishers=rest['value'];
+        console.log('publish',rest['value'])
+      })
+    
+      }
     })
       
     
