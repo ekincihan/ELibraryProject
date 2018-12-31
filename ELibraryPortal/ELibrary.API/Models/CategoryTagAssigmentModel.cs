fbc1dc0ee78b6fc5d7ddf04f680b21617484d3e3
esat <esat.avsr@gmail.com>
2018-12-31 19:03:35 +0300

    categorytag

--- ELibraryPortal/ELibrary.API/Models/CategoryTagAssigmentModel.cs
+++ ELibraryPortal/ELibrary.API/Models/CategoryTagAssigmentModel.cs
@@ -10,8 +10,22 @@ namespace ELibrary.API.Models
     public class CategoryTagAssigmentModel: IModelBase
     {
         public string id { get; set; }
-        public Guid BookId { get; set; }
-        public Guid CategoryId { get; set; }
+        public MongoBookModel BookId { get; set; }
+        public MongoCategoryModel CategoryId { get; set; }
         public List<Guid> Tags { get; set; }
+    }
+
+
+    public class MongoCategoryModel
+    {
+        public string Name{ get; set; }
+        public Guid CategoryId { get; set; }
+    }
+
+    public class MongoBookModel
+    {
+        public Guid BookId { get; set; }
+        public string BookName { get; set; }
+
     }
 }
--- ELibraryPortal/ELibrary.Portal/Models/BookPageModel.cs
+++ ELibraryPortal/ELibrary.Portal/Models/BookPageModel.cs
@@ -12,6 +12,8 @@ namespace ELibrary.Portal.Models
         public List<BookModel> BookList { get; set; }
         public List<PublisherModel> PubLisherList { get; set; }
         public List<AuthorModel> AuthorList { get; set; }
+        public List<CategoryModel> CategoryList { get; set; }
+        public List<TagModel> TagList { get; set; }
         public BookModel bookModel { get; set; }
         public IFormFile Thumbnail { get; set; }
         public IFormFile Publication { get; set; }
